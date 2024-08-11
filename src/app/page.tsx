dadb11052e0388632c11f328b1f0c63591f4c647
Syed Shurem Ali <160542322+shuremali02@users.noreply.github.com>
2024-08-11 12:38:32 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,12 @@ export default function Milestone1
     <br />
     <br />
     
-
+<div className="bg-yellow-300">
      <h1 className= "flex justify-center font-extrabold text-3xl animate-pulse text-blue-600 hover:uppercase hover:text-red-400 "> Hello </h1>
+  </div>
+   <div className="bg-green-600">  
      <h1 className= "flex justify-center font-extrabold text-4xl animate-bounce text-red-600 hover:uppercase hover:text-blue-400 "> World!</h1>
+     </div>
      <br /> 
      <br />
      <br />
